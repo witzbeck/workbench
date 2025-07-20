fd08b7facb14eeed44442b0900041f55f6fc30f9
Alex Beckwith <alex.beckwith@mmgins.com>
2025-07-20 08:52:23 -0400

    Enhance dbt project configuration: added materialization settings for the calendar model in dbt_project.yml and refactored asset definitions in definitions.py to streamline asset management.

--- src/workbench/defs/definitions.py
+++ src/workbench/defs/definitions.py
@@ -1,17 +1,16 @@
 from pathlib import Path
 from dagster import Definitions
 from dagster_dbt import dbt_assets
-
+from .assets import assets as py_assets
 
 
 project_path = Path(__file__).parent.parent
 project = DbtProject(project_dir=project_path)
+assets = py_assets + dbt_assets(
+    project=project,
+    select=["model.calendar.dates"],
+)
 
 defs = Definitions(
-    assets=[
-        dbt_assets(
-            project=project,
-            select=["model.calendar.dates"],
-        )
-    ]
+    assets=assets
 )
