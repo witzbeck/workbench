8cd956e1dde5d9dd74b415d8e566a355eaa9a1a7
Alex Beckwith <alex.beckwith@mmgins.com>
2025-07-27 09:09:34 -0400

    Remove unused DbtProject instantiation in definitions.py: eliminated the project variable to streamline the codebase and enhance maintainability.

--- src/workbench/defs/definitions.py
+++ src/workbench/defs/definitions.py
@@ -1,17 +1,11 @@
 
 from dagster import Definitions
-from dagster_dbt import DbtProject
-
-from workbench.constants import PROJECT_PATH
 
 from .asset_checks import asset_checks as py_asset_checks
 from .assets import assets
 from .jobs import jobs
 from .resources import resources
 from .schedules import schedules
-
-project = DbtProject(project_dir=PROJECT_PATH)
-
 
 defs = Definitions(
     assets=assets,
