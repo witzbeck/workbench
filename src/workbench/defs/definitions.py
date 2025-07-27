64b8bc7287ec9ffe4085ce54c78ed35010b7a9d2
Alex Beckwith <alex.beckwith@mmgins.com>
2025-07-27 08:48:55 -0400

    Refactor project path initialization: updated the DbtProject instantiation in definitions.py to use PROJECT_PATH for improved clarity, and modified database resource initialization in resources.py to use the POSIX path format, enhancing compatibility and maintainability.

--- src/workbench/defs/definitions.py
+++ src/workbench/defs/definitions.py
@@ -1,15 +1,15 @@
-from pathlib import Path
 
 from dagster import Definitions
 from dagster_dbt import DbtProject
+
+from workbench.constants import PROJECT_PATH
 
 from .asset_checks import asset_checks as py_asset_checks
 from .assets import assets
 from .resources import resources
 from .schedules import schedules
 
-project_path = Path(__file__).parent.parent
-project = DbtProject(project_dir=project_path)
+project = DbtProject(project_dir=PROJECT_PATH)
 
 
 defs = Definitions(
--- src/workbench/defs/resources.py
+++ src/workbench/defs/resources.py
@@ -6,8 +6,8 @@ from workbench.constants import DATA_PATH, PROJECT_PATH
 
 db_path = DATA_PATH / "workbench.db"
 
-workbench_db = DuckDBResource(database=db_path)
-workbench_io = DuckDBPandasIOManager(database=db_path)
+workbench_db = DuckDBResource(database=db_path.as_posix())
+workbench_io = DuckDBPandasIOManager(database=db_path.as_posix())
 dbt = DbtCliResource(project_dir=PROJECT_PATH, profiles_dir=PROJECT_PATH)
 resources = {
     "workbench_db": workbench_db,
