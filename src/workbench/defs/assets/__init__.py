9a50bf5480a8f8496ec53d881ea9d430b92e55c2
Alex Beckwith <alex.beckwith@mmgins.com>
2025-07-27 10:06:41 -0400

    Add asset metadata and implement custom DagsterDbtTranslator: enhanced sources.yml with asset_key for holidays and introduced CalendarGroupTranslator for improved asset grouping and auto materialization policy in __init__.py.

--- src/workbench/defs/assets/__init__.py
+++ src/workbench/defs/assets/__init__.py
@@ -1,10 +1,22 @@
-from dagster_dbt import DbtCliResource, dbt_assets
+from typing import Any, Dict
+
+from dagster import AutoMaterializePolicy
+from dagster_dbt import DagsterDbtTranslator, DbtCliResource, dbt_assets
+
+from workbench.constants import ASSET_GROUPS
 
 from ..projects import project
 from .calendars import assets as calendars_assets
 
 
-@dbt_assets(project=project, manifest=project.manifest_path)
+class CalendarGroupTranslator(DagsterDbtTranslator):
+    def get_group_name(self, node_info: Dict[str, Any]) -> str:
+        return ASSET_GROUPS.CALENDAR.value
+
+    def get_auto_materialize_policy(self, node_info: Dict[str, Any]) -> AutoMaterializePolicy:
+        return AutoMaterializePolicy.eager()
+
+@dbt_assets(project=project, manifest=project.manifest_path, dagster_dbt_translator=CalendarGroupTranslator)
 def dbt_calendar_assets(context, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
