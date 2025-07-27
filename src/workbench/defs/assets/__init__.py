3054bb9675dcd2a3c9624349405361db01352835
Alex Beckwith <alex.beckwith@mmgins.com>
2025-07-27 08:29:44 -0400

    Remove unused asset checks and DuckDB configuration: deleted asset checks for holiday assets and removed the DuckDB file, streamlining the codebase and improving maintainability.

--- src/workbench/defs/assets/__init__.py
+++ src/workbench/defs/assets/__init__.py
@@ -1,13 +1,13 @@
 from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
 
-from workbench.constants import ASSET_GROUPS, MODULE_PATH
+from workbench.constants import PROJECT_PATH
 
 from .calendars import assets as calendars_assets
 
-project = DbtProject(project_dir=MODULE_PATH)
+project = DbtProject(project_dir=PROJECT_PATH)
 project.prepare_if_dev()
 
-@dbt_assets(manifest=project.manifest_path, group_name=ASSET_GROUPS.CALENDAR.value)
+@dbt_assets(project=project, manifest=project.manifest_path)
 def dbt_calendar_assets(context, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
