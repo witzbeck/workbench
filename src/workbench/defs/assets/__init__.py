1405a3ffedb98b64976822de9f9dd718d6ab36c2
Alex Beckwith <alex.beckwith@mmgins.com>
2025-07-27 10:07:49 -0400

    Update dbt_assets decorator in __init__.py: instantiated CalendarGroupTranslator in the dbt_assets decorator to ensure proper initialization and enhance asset management.

--- src/workbench/defs/assets/__init__.py
+++ src/workbench/defs/assets/__init__.py
@@ -16,7 +16,7 @@ class CalendarGroupTranslator(DagsterDbtTranslator):
     def get_auto_materialize_policy(self, node_info: Dict[str, Any]) -> AutoMaterializePolicy:
         return AutoMaterializePolicy.eager()
 
-@dbt_assets(project=project, manifest=project.manifest_path, dagster_dbt_translator=CalendarGroupTranslator)
+@dbt_assets(project=project, manifest=project.manifest_path, dagster_dbt_translator=CalendarGroupTranslator())
 def dbt_calendar_assets(context, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
